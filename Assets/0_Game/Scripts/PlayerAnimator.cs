cbc59ae68417f56f2c1fb7f8a47ccd27016c1e5d
Jarl <Jarlo@bk.ru>
2021-12-14 15:03:14 +0500

    added test walking

---
+++ Assets/0_Game/Scripts/PlayerAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        private static readonly int X = Animator.StringToHash("X");
+        private static readonly int Y = Animator.StringToHash("Y");
+        
+        public void SetMoveValue(float x, float y)
+        {
+            animator.SetFloat(X, x);
+            animator.SetFloat(Y, y);
+        }
+    }
+}
---
+++ Assets/0_Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Scripts
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private PlayerAnimator animator;
+        
+        private void Update()
+        {
+            CheckInput();
+        }
+
+        private void CheckInput()
+        {
+            var vertical = Input.GetAxis("Vertical");
+            var horizontal = Input.GetAxis("Horizontal");
+            print(vertical);
+            animator.SetMoveValue(horizontal, vertical);
+        }
+    }
+}
